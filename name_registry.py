1c6784afe21a682651e9ead6c8cfa2d66c2a351b
Steve Ellis <email@steveell.is>
2015-08-07 01:56:42 -0400

    Better name registry

--- name_registry.py
+++ name_registry.py
@@ -1,13 +1,16 @@
 from pyethereum import tester, utils
 
+key = 'steve'
 s = tester.state()
-c = s.abi_contract('namecoin.se')
+registry = s.abi_contract('name_registry.se')
 public_key0 = utils.privtoaddr(tester.k0)
 public_key1 = utils.privtoaddr(tester.k1)
 
-c.register('steve', public_key0)
-c.register('steve', public_key1)
+registry.register(key, sender=tester.k0)
+registry.set_value(key, 'rules', sender=tester.k0)
 
-print(str(c.ask('steve')))
-print(str(public_key0))
-print(str(public_key0 == c.ask('steve')))
+registry.register(key, sender=tester.k1)
+registry.set_value(key, 'stinks', sender=tester.k1)
+
+print(key)
+print(str(registry.value(key)))
